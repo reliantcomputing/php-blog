3fcf4d837f15ea856f55021468ae22fdbc307d6f
Tumisho Mothobekhi <tumisho@psybergate.co.za>
2021-04-08 17:02:33 +0200

    Added post creation form

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,7 +2,30 @@
 
 @section('content')
     <div class="row">
-        <h3>Posts</h3> <hr>
+        @auth
+            <form method="POST" action='#'>
+                <div class="mb-2">
+                    <input type="text" name="title" class="form-control" placeholder="Enter title here..." />
+                </div>
+                @error('title')
+                    <div class="text-danger mt-1 mb-1">
+                        {{ $message }}
+                    </div>
+                @enderror
+                <div class="mb-2">
+                    <textarea class="form-control" name="body" placeholder="Write something here"></textarea>
+                </div>
+                @error('body')
+                    <div class="text-danger mt-1 mb-1">
+                        {{ $message }}
+                    </div>
+                @enderror
+                <div class="mb-2">
+                    <button type="submit" class="btn btn-primary">Create Post</button>
+                </div>
+            </form>
+        @endauth
+        <h3 class="mt-2 mb-2">Posts</h3>
         <div class="col-md-6">
             Posts
         </div>
